d5c1892738db3bb86630ae9805480fc7df835ad8
trmviet0801 <trmviet0801@gmail.com>
2025-06-05 21:48:47 +0700

    balance sheet model

---
+++ quantly-trading-service/models/balance_sheet.go
@@ -0,0 +1,19 @@
+package models
+
+type BalanceSheet struct {
+	StockSymbol                         string  `json:"stock_symbol"`
+	TotalAssets                         float64 `json:"total_assets"`
+	TotalLiabilitiesNetMinorityInterest float64 `json:"total_liabilities_net_minority_interest"`
+	TotalEquityGrossMinorityInterest    float64 `json:"total_equity_gross_minority_interest"`
+	TotalCapitalization                 float64 `json:"total_capitalization"`
+	CommonStockEquity                   float64 `json:"common_stock_equity"`
+	CapitalLeaseObligations             float64 `json:"capital_lease_obligations"`
+	NetTangibleAssets                   float64 `json:"net_tangible_assets"`
+	WorkingCapital                      float64 `json:"working_capital"`
+	InvestedCapital                     float64 `json:"invested_capital"`
+	TangibleBookValue                   float64 `json:"tangible_book_value"`
+	TotalDebt                           float64 `json:"total_debt"`
+	NetDebt                             float64 `json:"net_debt"`
+	ShareIssued                         float64 `json:"share_issued"`
+	OrdinarySharesNumber                float64 `json:"ordinary_shares_number"`
+}
