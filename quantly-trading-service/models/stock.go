43484a81eb99163f0b41dfa7f7ebb7e488fec697
trmviet0801 <trmviet0801@gmail.com>
2025-06-05 15:43:19 +0700

    set up stock entity

--- quantly-trading-service/models/stock.go
+++ quantly-trading-service/models/stock.go
@@ -1,4 +1,26 @@
 package models
 
 type Stock struct {
+	Name            string  `binding:"required" json:"name"`
+	Symbol          string  `binding:"required" json:"symbol"`
+	IpoYear         int16   `json:"ipo_year"`
+	Country         string  `binding:"required" json:"country"`
+	CurrentPrice    float64 `json:"current_price"`
+	PriceChange     float64 `json:"price_change"`
+	ChangePercent   float32 `json:"change_percent"`
+	OpenPrice       float64 `json:"open_price"`
+	DayRange        float64 `json:"day_range"`
+	DayLow          float64 `json:"day_low"`
+	DayHigh         float64 `json:"day_high"`
+	Volume          int64   `json:"volume"`
+	LatestTradeTime string  `json:"latest_trade_time"`
+	Ticker          string  `json:"ticker"`
+	Exchange        string  `json:"exchange"`
+	Industry        string  `binding:"required" json:"industry"`
+	Sector          string  `binding:"required" json:"sector"`
+	Employees       int32   `json:"employees"`
+	Headquarters    string  `json:"headquarters"`
+	MarketCap       float64 `json:"market_cap"`
+	PERatioTtm      float32 `json:"pe_ratio_ttm"`
+	EPSTtm          float32 `json:"eps_ttm"`
 }
