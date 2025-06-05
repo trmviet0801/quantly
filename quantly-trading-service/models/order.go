89ee9b3a4cf7b446a79d3b903236e2ff8031e636
trmviet0801 <trmviet0801@gmail.com>
2025-06-05 23:25:23 +0700

    order model

---
+++ quantly-trading-service/models/order.go
@@ -0,0 +1,19 @@
+package models
+
+type Order struct {
+	OrderId       int64   `json:"order_id" binding:"required"`
+	AccountId     int64   `json:"account_id" binding:"required"`
+	Symbol        string  `json:"symbol" binding:"required"`
+	Quantity      float64 `json:"qty" binding:"required"`
+	Notional      float64 `json:"notional"`
+	Side          string  `json:"side"`
+	Type          string  `json:"type"`
+	TimeInForce   string  `json:"time_in_force"`
+	LimitPrice    float64 `json:"limit_price"`
+	StopPrice     float64 `json:"stop_price"`
+	TrailPrice    float64 `json:"trail_price"`
+	TrailPercent  float64 `json:"trail_percent"`
+	ExtendedHours bool    `json:"extended_hours"`
+	ClientOrderId string  `json:"client_order_id"`
+	OrderClass    string  `json:"order_class"`
+}
