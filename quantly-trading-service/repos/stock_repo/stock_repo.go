e1961f077284fe8d6cd526a0342b097750458f1b
trmviet0801 <trmviet0801@gmail.com>
2025-06-05 16:09:43 +0700

    set up stock repo

---
+++ quantly-trading-service/repos/stock_repo/stock_repo.go
@@ -0,0 +1,125 @@
+package stockrepo
+
+import (
+	"database/sql"
+	"errors"
+
+	"github.com/trmviet0801/quantly/models"
+)
+
+type StockRepo struct {
+	db *sql.DB
+}
+
+func (stockRepo *StockRepo) GetById(id string) (stock *models.Stock, err error) {
+	query := `SELECT name, symbol, ipo_year, country, current_price, price_change, change_percent,
+       open_price, day_range, day_low, day_high, volume, latest_trade_time, ticker,
+       exchange, industry, sector, employees, headquarters, market_cap, pe_ratio_ttm, eps_ttm FROM stocks WHERE symbol = ?`
+	stock = &models.Stock{}
+	err = stockRepo.db.QueryRow(query, id).Scan(
+		&stock.Name,
+		&stock.Symbol,
+		&stock.IpoYear,
+		&stock.Country,
+		&stock.CurrentPrice,
+		&stock.PriceChange,
+		&stock.ChangePercent,
+		&stock.OpenPrice,
+		&stock.DayRange,
+		&stock.DayLow,
+		&stock.DayHigh,
+		&stock.Volume,
+		&stock.LatestTradeTime,
+		&stock.Ticker,
+		&stock.Exchange,
+		&stock.Industry,
+		&stock.Sector,
+		&stock.Employees,
+		&stock.Headquarters,
+		&stock.MarketCap,
+		&stock.PERatioTtm,
+		&stock.EPSTtm,
+	)
+	if err != nil {
+		return nil, errors.New("stock not found")
+	}
+	return stock, nil
+}
+
+func (stockRepo *StockRepo) Create(stock *models.Stock) error {
+	query := `INSERT INTO stocks (name, symbol, ipo_year, country, current_price, price_change, change_percent,
+	   open_price, day_range, day_low, day_high, volume, latest_trade_time, ticker,
+	   exchange, industry, sector, employees, headquarters, market_cap, pe_ratio_ttm, eps_ttm)
+	   VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
+	_, err := stockRepo.db.Exec(query,
+		stock.Name,
+		stock.Symbol,
+		stock.IpoYear,
+		stock.Country,
+		stock.CurrentPrice,
+		stock.PriceChange,
+		stock.ChangePercent,
+		stock.OpenPrice,
+		stock.DayRange,
+		stock.DayLow,
+		stock.DayHigh,
+		stock.Volume,
+		stock.LatestTradeTime,
+		stock.Ticker,
+		stock.Exchange,
+		stock.Industry,
+		stock.Sector,
+		stock.Employees,
+		stock.Headquarters,
+		stock.MarketCap,
+		stock.PERatioTtm,
+		stock.EPSTtm,
+	)
+	if err != nil {
+		return errors.New("failed to create stock")
+	}
+	return nil
+}
+
+func (stockRepo *StockRepo) Update(stock *models.Stock) error {
+	query := `UPDATE stocks SET name = ?, ipo_year = ?, country = ?, current_price = ?, price_change = ?,
+	   change_percent = ?, open_price = ?, day_range = ?, day_low = ?, day_high = ?, volume = ?,
+	   latest_trade_time = ?, ticker = ?, exchange = ?, industry = ?, sector = ?, employees = ?,
+	   headquarters = ?, market_cap = ?, pe_ratio_ttm = ?, eps_ttm = ? WHERE symbol = ?`
+	_, err := stockRepo.db.Exec(query,
+		stock.Name,
+		stock.IpoYear,
+		stock.Country,
+		stock.CurrentPrice,
+		stock.PriceChange,
+		stock.ChangePercent,
+		stock.OpenPrice,
+		stock.DayRange,
+		stock.DayLow,
+		stock.DayHigh,
+		stock.Volume,
+		stock.LatestTradeTime,
+		stock.Ticker,
+		stock.Exchange,
+		stock.Industry,
+		stock.Sector,
+		stock.Employees,
+		stock.Headquarters,
+		stock.MarketCap,
+		stock.PERatioTtm,
+		stock.EPSTtm,
+		stock.Symbol)
+	if err != nil {
+		return errors.New("failed to update stock")
+	}
+	return nil
+}
+
+func (stockRepo *StockRepo) DeleteById(id string) error {
+	query := `DELETE FROM stocks WHERE symbol = ?`
+	_, err := stockRepo.db.Exec(query, id)
+	if err != nil {
+		return errors.New("failed to delete stock")
+	}
+	return nil
+}
