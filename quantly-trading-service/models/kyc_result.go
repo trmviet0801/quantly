647547cf4421a00a528c2910a1c651bce90a72a7
trmviet0801 <trmviet0801@gmail.com>
2025-06-09 03:26:09 +0700

    kycResult model

---
+++ quantly-trading-service/models/kyc_result.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type KycResult struct {
+	KycResultId    int64     `json:"kyc_result_id" gorm:"primaryKey;autoIncrement"`
+	AccountId      int64     `json:"account_id" gorm:"foreignKey:AccountId;references:AccountId;not null"`
+	Status         string    `json:"status"`
+	InternalStatus string    `json:"internal_status"`
+	TimeStamp      time.Time `json:"timestamp"`
+}
