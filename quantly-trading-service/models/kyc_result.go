9eb55ac90598931660982db28bd9833e84e847f4
trmviet0801 <trmviet0801@gmail.com>
2025-06-09 03:35:45 +0700

    update kycResultModel

--- quantly-trading-service/models/kyc_result.go
+++ quantly-trading-service/models/kyc_result.go
@@ -3,7 +3,7 @@ package models
 import "time"
 
 type KycResult struct {
-	KycResultId    int64     `json:"kyc_result_id" gorm:"primaryKey;autoIncrement"`
+	KycResultId    int64     `json:"kyc_result_id" gorm:"primaryKey"`
 	AccountId      int64     `json:"account_id" gorm:"foreignKey:AccountId;references:AccountId;not null"`
 	Status         string    `json:"status"`
 	InternalStatus string    `json:"internal_status"`
