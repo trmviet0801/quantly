2e7e5f9d965585cce47be9595118cbdb133505c5
trmviet0801 <trmviet0801@gmail.com>
2025-06-09 02:44:54 +0700

    tradingConfiguration model

---
+++ quantly-trading-service/models/trading_configuration.go
@@ -0,0 +1,10 @@
+package models
+
+type TradingConfiguration struct {
+	TradingConfigurationId int64  `json:"trading_configuration_id" binding:"required" gorm:"primaryKey"`
+	AccountId              int64  `json:"account_id" binding:"required" gorm:"foreignKey:AccountId;references:AccountId"`
+	DtbpCheck              string `json:"dtbp_check" binding:"required"`            // "true" or "false"
+	NoShhorting            bool   `json:"no_shorting" binding:"required"`           // "true" or "false"
+	FractionalTrading      bool   `json:"fractional_trading" binding:"required"`    // "true" or "false"
+	MaxMarginMultiplier    string `json:"max_margin_multiplier" binding:"required"` // "0.5", "1.0", etc.
+}
