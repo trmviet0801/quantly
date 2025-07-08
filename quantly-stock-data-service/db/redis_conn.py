19dda3345c4e1139226ad5d6d8f6b140df57c70e
trmviet0801 <trmviet0801@gmail.com>
2025-07-08 11:17:38 +0700

    refactor

--- quantly-stock-data-service/db/redis_conn.py
+++ quantly-stock-data-service/db/redis_conn.py
@@ -9,8 +9,8 @@ load_dotenv()
 def get_redis_instance():
     global _redis_instance
     if _redis_instance is None:
-        _redis_instance = redis.Redis(host='redis-12822.c93.us-east-1-3.ec2.redns.redis-cloud.com',
-                                      port=12822,
+        _redis_instance = redis.Redis(host=os.getenv('REDIS_HOST'),
+                                      port=int(os.getenv('REDIS_PORT')),
                                       decode_responses=True,
                                       username=os.getenv('REDIS_USERNAME'),
                                       password=os.getenv('REDIS_PASSWORD'))
